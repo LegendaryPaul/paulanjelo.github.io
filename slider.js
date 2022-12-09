7404695fe4c71828dd29a24c41d998b4c10cc985
LegendaryPaul <113983581+LegendaryPaul@users.noreply.github.com>
2022-12-09 21:38:45 +0800

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,16 @@
+const workContainers = [...document.querySelectorAll('.work-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+workContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    });
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    });
+});
